d41de0d1a22b3afbc1f39670814c4c7387b81f13
azaria amanda <azaryaamanda@gmail.com>
2024-11-26 00:43:18 +0700

    Percobaan dan modifikasi jobsheet 10

--- Bioskop02.java
+++ Bioskop02.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Bioskop02 {
     public static void main(String[] args) {
         String[][] penonton = new String[4][2];
@@ -16,19 +17,23 @@ public class Bioskop02 {
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
         
+        // System.out.println(penonton.length);
+        // System.out.println(penonton[0].length);
+        // System.out.println(penonton[1].length);
+        // System.out.println(penonton[2].length);
+        // System.out.println(penonton[3].length);
+
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
 
-        // System.out.println("Penonton pada baris ke-3 ");
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
 
+        System.out.println("Penonton pada baris ke-3: ");
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ",penonton[i]));
         }
-        // for (String[] barisPenonton : penonton) {
-        //     System.out.println("Panjang baris: " + barisPenonton.length);
-        // }
+
+        
     }
 }
